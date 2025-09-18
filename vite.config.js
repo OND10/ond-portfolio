4534a2763683b7427b5bfa4e076874993187694a
OND <106841560+OND10@users.noreply.github.com>
2025-09-18 13:33:50 +0300

    change base url to match new hosting service

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ond10.github.io/ond-portfolio/',
+   base: process.env.VITE_BASE_PATH || "/ond-portfolio",
 })
