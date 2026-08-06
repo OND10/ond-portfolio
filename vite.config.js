8793239a8a5a0407873ec695e0746a2c25da2ea6
OND10 <osamadammag84@gmail.com>
2026-08-07 00:05:05 +0300

    vite and meta data of portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/* global process */
 export default defineConfig({
   plugins: [react()],
    base: process.env.VITE_BASE_PATH || "/ond-portfolio",
